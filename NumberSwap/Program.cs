d936028bbcee4edde4008084147065a49b3e793c
Vishen_Manthan <90868958+Themanthanvishen@users.noreply.github.com>
2022-08-02 04:38:47 -0400

    Add project files.

---
+++ NumberSwap/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class SwapExe
+{
+    public static void Main(string[] args)
+    {
+        int number1;
+        int number2;
+        int sawpnum;
+
+        Console.WriteLine("\nWelcome!");
+        Console.Write("\nFirst Number : ");
+
+        number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("\nSecond Number : ");
+
+        number2 = int.Parse(Console.ReadLine());
+
+        sawpnum = number1;
+        number1 = number2;
+        number2 = sawpnum;
+
+        
+        Console.Write("\nFirst Number : " + number1);
+        Console.Write("\nSecond Number : " + number2);
+        Console.ReadLine();
+
+    }
+}
